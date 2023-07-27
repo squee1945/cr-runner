449311897f96ec024c562be1effba1986d1f57bc
Jason Collins <jason.a.collins@gmail.com>
2023-07-27 12:57:20 -0700

    Update

--- webhook/apphandler.go
+++ webhook/apphandler.go
@@ -86,7 +86,7 @@ func (h apphandler) parseIntParam(name string) (int64, error) {
 	if s == "" {
 		return 0, fmt.Errorf("%s is required", name)
 	}
-	i, err := strconv.ParseInt(s, 10)
+	i, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
 		return 0, fmt.Errorf("%s must be integer: %v", name, err)
 	}
