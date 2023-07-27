d0044fe11ab36b442475446fc7ffcccacd8f2e15
Jason Collins <jason.a.collins@gmail.com>
2023-07-27 14:38:25 -0700

    Update

--- webhook/reg.go
+++ webhook/reg.go
@@ -34,12 +34,11 @@ func (r Registration) Token(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("generating JWT: %v", err)
 	}
-	// appToken, err := r.appAccessToken(ctx, jwt)
-	// if err != nil {
-	// 	return "", fmt.Errorf("generating app access token: %v", err)
-	// }
-	// regToken, err := r.appRegistrationToken(ctx, appToken)
-	regToken, err := r.appRegistrationToken(ctx, jwt)
+	appToken, err := r.appAccessToken(ctx, jwt)
+	if err != nil {
+		return "", fmt.Errorf("generating app access token: %v", err)
+	}
+	regToken, err := r.appRegistrationToken(ctx, appToken)
 	if err != nil {
 		return "", fmt.Errorf("generating app registration token: %v", err)
 	}
@@ -57,42 +56,42 @@ func (r Registration) generateJWT(ctx context.Context) (string, error) {
 	return token.SignedString(r.pk)
 }
 
-// func (r Registration) appAccessToken(ctx context.Context, jwt string) (string, error) {
-// 	url := fmt.Sprintf("https://api.github.com/app/installations/%d/access_tokens", r.installationID)
-// 	req, err := http.NewRequest(http.MethodPost, url, nil)
-// 	if err != nil {
-// 		return "", fmt.Errorf("creating http request: %v", err)
-// 	}
-// 	req.Header.Add("Accept", "application/vnd.github+json")
-// 	req.Header.Add("Authorization", "Bearer "+jwt)
-// 	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+func (r Registration) appAccessToken(ctx context.Context, jwt string) (string, error) {
+	url := fmt.Sprintf("https://api.github.com/app/installations/%d/access_tokens", r.installationID)
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return "", fmt.Errorf("creating http request: %v", err)
+	}
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("Authorization", "Bearer "+jwt)
+	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
 
-// 	res, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		return "", fmt.Errorf("calling access_tokens API (status %d): %v", res.StatusCode, err)
-// 	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("calling access_tokens API (status %d): %v", res.StatusCode, err)
+	}
 
-// 	tokenBytes, err := io.ReadAll(res.Body)
-// 	if err != nil {
-// 		return "", fmt.Errorf("reading access_tokens response body: %v", err)
-// 	}
-// 	logInfo("Access token response: " + string(tokenBytes))
+	tokenBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("reading access_tokens response body: %v", err)
+	}
+	logInfo("Access token response: " + string(tokenBytes))
 
-// 	type ghToken struct {
-// 		Token              string            `json:"token"`
-// 		ExpiresAt          time.Time         `json:"expires_at"`
-// 		Permissions        map[string]string `json:"permissions"`
-// 		RepositorySelected string            `json:"repository_selection"`
-// 	}
-// 	var ght ghToken
-// 	if err := json.Unmarshal(tokenBytes, &ght); err != nil {
-// 		return "", fmt.Errorf("unmarshalling response: %v", err)
-// 	}
-// 	if ght.Token == "" {
-// 		return "", errors.New("token was empty")
-// 	}
-// 	return ght.Token, nil
-// }
+	type ghToken struct {
+		Token              string            `json:"token"`
+		ExpiresAt          time.Time         `json:"expires_at"`
+		Permissions        map[string]string `json:"permissions"`
+		RepositorySelected string            `json:"repository_selection"`
+	}
+	var ght ghToken
+	if err := json.Unmarshal(tokenBytes, &ght); err != nil {
+		return "", fmt.Errorf("unmarshalling response: %v", err)
+	}
+	if ght.Token == "" {
+		return "", errors.New("token was empty")
+	}
+	return ght.Token, nil
+}
 
 func (r Registration) appRegistrationToken(ctx context.Context, appAccessToken string) (string, error) {
 	// https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-a-repository
@@ -102,7 +101,7 @@ func (r Registration) appRegistrationToken(ctx context.Context, appAccessToken s
 		return "", fmt.Errorf("creating http request: %v", err)
 	}
 	req.Header.Add("Accept", "application/vnd.github+json")
-	req.Header.Add("Authorization", "Bearer "+appAccessToken)
+	req.Header.Add("Authorization", "token "+appAccessToken)
 	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
 
 	res, err := http.DefaultClient.Do(req)
